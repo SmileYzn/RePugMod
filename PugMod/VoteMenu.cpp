ce1ad54ad62f0a176f616f031344073f838fa898
Cleverson <SmileYzn@users.noreply.github.com>
2022-05-02 17:49:43 -0300

    Added Vote Menu system
    Added Vote Menu system, only votekick is allowed for now

--- PugMod/VoteMenu.cpp
+++ PugMod/VoteMenu.cpp
@@ -2,6 +2,17 @@
 
 CVoteMenu gVoteMenu;
 
+void CVoteMenu::ClientGetIntoGame(CBasePlayer* Player)
+{
+	int EntityIndex = Player->entindex();
+
+	for (int i = 1; i <= gpGlobals->maxClients; ++i)
+	{
+		this->m_VoteKick[i][EntityIndex] = false;
+		this->m_VoteKick[EntityIndex][i] = false;
+	}
+}
+
 bool CVoteMenu::CheckMenu(CBasePlayer* Player)
 {
 	if (Player->m_iTeam == TERRORIST || Player->m_iTeam == CT)
@@ -72,32 +83,102 @@ void CVoteMenu::MenuHandle(int EntityIndex, int ItemIndex, const char* Option)
 	}
 }
 
-void CVoteMenu::VoteKick(CBasePlayer * Player)
+void CVoteMenu::VoteKick(CBasePlayer* Player)
 {
 	if (this->CheckMenu(Player))
 	{
-		//
+		CBasePlayer* Players[32] = { NULL };
+
+		int Num = gPlayer.GetList(Players,Player->m_iTeam);
+
+		int NeedPlayers = (gCvars.GetPlayersMin()->value / 2);
+
+		int EntityIndex = Player->entindex();
+
+		if (Num >= NeedPlayers)
+		{
+			gMenu[EntityIndex].Create("Vote Kick:", true, this->MenuHandle);
+
+			for (int i = 0;i < Num;i++)
+			{
+				if (EntityIndex != Players[i]->entindex())
+				{
+					if (!gAdmin.Check(Player->edict()))
+					{
+						gMenu[EntityIndex].AddItem(Players[i]->entindex(), STRING(Players[i]->edict()->v.netname));
+					}
+				}
+			}
+
+			gMenu[EntityIndex].Show(EntityIndex);
+
+			gUtil.SayText(Player->edict(), EntityIndex, "Choose a player to vote."); 
+		}
+		else
+		{
+			gUtil.SayText(Player->edict(), EntityIndex, "Need \3%d\1 players to use vote kick.", NeedPlayers);
+		}
+	}
+}
+
+void CVoteMenu::VoteKick(CBasePlayer* Player, CBasePlayer* Target)
+{
+	this->m_VoteKick[Player->entindex()][Target->entindex()] = true;
+
+	int VoteCount = 0;
+
+	for (int i = 1; i <= gpGlobals->maxClients; ++i)
+	{
+		if (this->m_VoteKick[i][Target->entindex()])
+		{
+			VoteCount++;
+		}
+	}
+
+	int VotesNeed = (gPlayer.GetNum(Player->m_iTeam) - 1);
+	int VotesLack = (VotesNeed - VoteCount);
+
+	if (!VotesLack)
+	{
+		gUtil.ServerCommand("kick #%d \"Kicked by Vote Kick.\"", GETPLAYERUSERID(Target->edict()));
+
+		gUtil.SayText(NULL, Target->entindex(), "\3%s\1 Kicked: \4%d\1 votes reached.", STRING(Target->edict()->v.netname), VotesNeed);
+	}
+	else
+	{
+		gUtil.SayText(NULL, Player->entindex(), "\3%s\1 voted to kick %s: Need more \4%d\1 votes to kick.", STRING(Target->edict()->v.netname), VotesNeed);
 	}
 }
 
 void CVoteMenu::VoteKickHandle(int EntityIndex, int ItemIndex, const char * Option)
 {
+	auto Player = UTIL_PlayerByIndexSafe(EntityIndex);
+
+	if (Player)
+	{
+		auto Target = UTIL_PlayerByIndexSafe(ItemIndex);
+
+		if (Target)
+		{
+			gVoteMenu.VoteKick(Player, Target);
+		}
+	}
 }
 
-void CVoteMenu::VoteMap(CBasePlayer * Player)
+void CVoteMenu::VoteMap(CBasePlayer* Player)
 {
 	if (this->CheckMenu(Player))
 	{
-		//
+		// TODO: Vote  Map Menu
 	}
 }
 
 void CVoteMenu::VoteMapHandle(int EntityIndex, int ItemIndex, const char * Option)
 {
-	//
+	// TODO: Vote  Map Menu Handle
 }
 
-void CVoteMenu::VotePause(CBasePlayer * Player)
+void CVoteMenu::VotePause(CBasePlayer* Player)
 {
 	if (this->CheckMenu(Player))
 	{
@@ -109,7 +190,7 @@ void CVoteMenu::VotePauseHandle(int EntityIndex, int ItemIndex, const char * Opt
 {
 }
 
-void CVoteMenu::VoteStop(CBasePlayer * Player)
+void CVoteMenu::VoteStop(CBasePlayer* Player)
 {
 	if (this->CheckMenu(Player))
 	{
--- PugMod/VoteMenu.h
+++ PugMod/VoteMenu.h
@@ -3,12 +3,15 @@
 class CVoteMenu
 {
 public:
+	void ClientGetIntoGame(CBasePlayer* Player);
+
 	bool CheckMenu(CBasePlayer* Player);
 
 	void Menu(CBasePlayer* Player);
 	static void MenuHandle(int EntityIndex, int ItemIndex, const char* Option);
 
 	void VoteKick(CBasePlayer* Player);
+	void VoteKick(CBasePlayer* Player, CBasePlayer* Target);
 	static void VoteKickHandle(int EntityIndex, int ItemIndex, const char* Option);
 
 	void VoteMap(CBasePlayer* Player);
@@ -21,6 +24,7 @@ public:
 	static void VoteStopHandle(int EntityIndex, int ItemIndex, const char* Option);
 
 private:
+	bool m_VoteKick[33][33];
 };
 
 extern CVoteMenu gVoteMenu;
