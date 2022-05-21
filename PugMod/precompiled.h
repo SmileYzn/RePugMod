6013cf299f6fd17d087d559b89184fcc412946a2
Cleverson <smileyznl@gmail.com>
2022-05-20 22:42:38 -0300

    Linux build fixes
    Linux build fixes

--- PugMod/precompiled.h
+++ PugMod/precompiled.h
@@ -5,15 +5,26 @@
 
 // If is not MSVC build
 #ifndef _WIN32
-#define _stricmp	strcasecmp
-#define _strnicmp	strncasecmp
-#define _strdup		strdup
-#define _unlink		unlink
-#define _vsnprintf	vsnprintf
-#define _write		write
-#define _close		close
-#define _access		access
-#define _vsnwprintf	vswprintf
+#define REGAMEDLL_FIXES
+#define REGAMEDLL_API
+#define REGAMEDLL_ADD
+#define UNICODE_FIXES
+#define BUILD_LATEST
+#define CLIENT_WEAPONS
+#define USE_QSTRING
+#define _LINUX
+#define LINUX
+#define NDEBUG
+#define _GLIBCXX_USE_CXX11_ABI		0
+#define _stricmp					strcasecmp
+#define _strnicmp					strncasecmp
+#define _strdup						strdup
+#define _unlink						unlink
+#define _vsnprintf					vsnprintf
+#define _write						write
+#define _close						close
+#define _acces						access
+#define _vsnwprintf					vswprintf
 #endif
 
 // System Includes
