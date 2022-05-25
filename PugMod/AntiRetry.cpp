edc750d0dad50b712843818af973a22aa977135e
Cleverson <smileyznl@gmail.com>
2022-05-25 14:25:15 -0300

    Fixed anti retry system
    Check teams, admin access before apply anti retry function.
    
    Added to ban players that disconnect in a live game when pug_reconnect_ban_time is greater than zero

--- PugMod/AntiRetry.cpp
+++ PugMod/AntiRetry.cpp
@@ -29,17 +29,35 @@ void CAntiRetry::ClientDisconnected(edict_t* pEntity, bool crash, const char* Re
 {
 	if (pEntity)
 	{
-		if (gCvars.GetReconnectDelay()->value > 0)
+		if (FStrEq(Reason, "Client sent 'drop'") || FStrEq(Reason, "Timed out"))
 		{
-			if (FStrEq(Reason, "Client sent 'drop'") || FStrEq(Reason, "Timed out"))
+			if (gCvars.GetReconnectDelay()->value > 0)
 			{
 				const char* Auth = GETPLAYERAUTHID(pEntity);
 
-				if (Auth)
+				if (!gAdmin.Check(Auth))
 				{
-					this->m_Data.erase(Auth);
+					auto Player = UTIL_PlayerByIndexSafe(ENTINDEX(pEntity));
 
-					this->m_Data.insert(std::make_pair(Auth, time(NULL) + (int)gCvars.GetReconnectDelay()->value));
+					if (Player)
+					{
+						if (Player->m_iTeam == TERRORIST || Player->m_iTeam == CT)
+						{
+							if (gCvars.GetReconnectBanTime()->value > 0)
+							{
+								if (gPugMod.GetState() >= PUG_STATE_FIRST_HALF && gPugMod.GetState() <= PUG_STATE_OVERTIME)
+								{
+									gPlayer.BanClient(Player->entindex(), (int)gCvars.GetReconnectBanTime()->value, false);
+								}
+							}
+							else
+							{
+								this->m_Data.erase(Auth);
+
+								this->m_Data.insert(std::make_pair(Auth, time(NULL) + (int)gCvars.GetReconnectDelay()->value));
+							}
+						}
+					}
 				}
 			}
 		}
